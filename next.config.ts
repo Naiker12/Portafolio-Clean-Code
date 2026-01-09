18cdaa7d71d8c93b7ff573f0e40f494972f2540d
naiker12 <gomeznaiker@gmail.com>
2026-01-09 14:52:27 -0500

    Despliegue

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   basePath: "/Portafolio-Clean-Code",
-  assetPrefix: "/Portafolio-Clean-Code",
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
